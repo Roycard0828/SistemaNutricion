71821bce6cbfbc0ef3bbb73a81516ce7d27e2f40
Roycard0828 <gamr_0111@outlook.es>
2022-09-26 08:40:11 -0500

    Finish the patients logic #2

--- Project/software_nutricion/patients/urls.py
+++ Project/software_nutricion/patients/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import create_patient, get_patient
+from .views import create_patient, get_patient, delete_patient, update_patients, get_all_patients
 
 urlpatterns = [
-    path('create/', create_patient, name='create'),
+    path('create/', create_patient, name='create_patient'),
     path('patient/<int:pk>', get_patient, name='get_patient'),
+    path('delete/<int:pk>', delete_patient, name='delete_patient'),
+    path('', get_all_patients, name='patients'),
+    path('update_patient/<int:pk>', update_patients, name='update_patient'),
 ]
--- Project/software_nutricion/patients/views.py
+++ Project/software_nutricion/patients/views.py
@@ -14,12 +14,54 @@ def create_patient(request):
         form = PatientForm(request.POST)
         if form.is_valid():
             patient = form.save()
-            return redirect('get_patient', patient.id)
+            return redirect('patients')
 
     context = {'form':form}
-    return render(request, 'patients/create_patient.html', context)
+    return render(request, 'patients/patient_form.html', context)
 
 def get_patient(request, pk):
     patient = Patient.objects.get(pk=pk)
     context = {'patient':patient}
     return render(request, 'patients/patient_profile.html', context)
+
+def delete_patient(request, pk):
+
+    try:
+        patient = Patient.objects.get(pk=pk)
+
+        if request.method == 'POST':                  
+            patient.delete()                
+            return redirect('patients') # NOTA: redigir al main
+
+        context = {'object':patient}
+    except:
+        context = {'object':None}
+
+    return render(request, 'patients/delete_patient.html', context)
+
+
+def update_patients(request, pk):
+        
+    try:
+        patient = Patient.objects.get(pk=pk)
+        form = PatientForm(instance=patient)
+
+        if request.method == 'POST':
+            form = PatientForm(request.POST, instance=patient)
+
+            if form.is_valid():
+                form.save()
+                return redirect('patients')
+    except:
+        return HTTPResponse('Patient not found')
+
+    context = {'form':form}
+
+    return render(request, 'patients/patient_form.html', context)
+
+def get_all_patients(request):
+    patients = Patient.objects.all()
+    
+    context = {'patients': patients}
+
+    return render(request, 'patients/patients.html', context)
